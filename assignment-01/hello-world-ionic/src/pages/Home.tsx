2a40ea02bc4817ab86a1d7db5402c089ed6f4b05
anangkf_ <gonanggoneng@gmail.com>
2023-03-21 21:32:49 +0700

    update: hello world ionic

--- assignment-01/hello-world-ionic/src/pages/Home.tsx
+++ assignment-01/hello-world-ionic/src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Hello World Ionic</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
