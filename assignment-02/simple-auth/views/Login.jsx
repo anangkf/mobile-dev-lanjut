cee1bd1e173b61a1b068381f8a1da039dcbfc47d
anangkf_ <gonanggoneng@gmail.com>
2023-04-07 20:32:57 +0700

    added login view

--- assignment-02/simple-auth/views/Login.jsx
+++ assignment-02/simple-auth/views/Login.jsx
@@ -1,20 +1,87 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a4eef7',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 28
   },
+  box: {
+    alignItems: 'center',
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    width: '100%',
+    minHeight: 300,
+    paddingVertical: 12,
+    paddingHorizontal: 38,
+    gap: 6
+  },
+  input: {
+    minWidth: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 4 ,
+    padding: 4
+  }
 });
 
+const INIT_FORM_DATA = {
+  email: '',
+  password: ''
+}
 
 const Login = () => {
+  const [formData, setFormData] = useState(INIT_FORM_DATA)
+
+  const handleChange = (value, name) => {
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = () => {
+    Alert.alert(`email: ${formData.email}\npassword: ${formData.password}`)
+    setFormData(INIT_FORM_DATA)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box}>
+        <Text style={{ fontSize: 28 }} >Login</Text>
+          <SafeAreaView style={{ gap: 8 }}>
+            <View>
+              <Text>Email</Text>
+              <TextInput 
+                style={styles.input} 
+                placeholder='email' 
+                value={formData.email} 
+                onChangeText={(value) => handleChange(value, 'email')} 
+                keyboardType='email-address' 
+                autoCapitalize='none' 
+                autoFocus 
+                autoComplete='email' 
+              />
+            </View>
+            <View>
+              <Text>Password</Text>
+              <TextInput 
+                style={styles.input} 
+                placeholder='password' 
+                value={formData.password} 
+                onChangeText={(value) => handleChange(value, 'password')} 
+                secureTextEntry={true} 
+                autoCapitalize='none' 
+                autoComplete='password' 
+              />
+            </View>
+            <Button title='Login' onPress={handleSubmit} />
+          </SafeAreaView>
+      </View>
     </View>
   )
 }
